a2cb081133f9b5272d46aceba7082635d2cd188e
Xan <xandrabest@gmail.com>
2025-06-16 20:29:31 -0500

    try changing the code

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Cowsayphp\Farm;
 
 header('Content-Type: text/plain');
 
-$text = "Set a message by adding ?message=<message here> to the URL";
+$text = "I can accept a message using ?message=<message here>";
 if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
 }
